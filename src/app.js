180792feb9ba04b350b9fac8436e53c1eef6389b
Asif Ahmed <llvllohd@gmail.com>
2020-03-10 13:28:31 +0530

    adding to gh-pages

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const inputfrom = document.querySelector('#from').value;
+// const tagfrom = document.querySelector('#tagFrom');
+const inputto = document.querySelector('#to');
+const tagto = document.querySelector('#tagTo');
+const inputsub = document.querySelector('#subject');
+const inputbody = document.querySelector('#body');
+const button = document.querySelector('#button');
+
+inputto.addEventListener("keypress",function(e){
+
+    if(e.which === 13){
+        const span = document.createElement("span");
+        span.textContent = ` ${e.target.value}`;
+        tagto.append(span);
+        tagto.style.display= "block";
+        inputto.value ="";
+
+//         let objectbox = document.querySelector('#msg pre');
+// objectbox.textContent = '\n' + JSON.stringify(inputs,'\t',1);
+
+    }
+});
+
+
+button.addEventListener('click',function(e){
+    // e.preventDefault;
+// console.log(e.target.value);
+let data = [];
+let inputs = {
+    from: document.querySelector('#from').value,
+    to: tagto.textContent,
+    subject: document.querySelector('#subject').value,
+    body: document.querySelector('#body').value
+}
+data.push(inputs);
+// document.forms[0].reset;
+let objectbox = document.querySelector('#msg pre');
+objectbox.textContent = '\n' + JSON.stringify(inputs,'\t',1);
+// console.log({data});
+
+
+});
